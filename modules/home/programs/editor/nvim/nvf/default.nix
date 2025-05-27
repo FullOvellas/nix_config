cda5dfc87eb3b7d4b0bf82c6e5367db5be94cd41
Sergio Alonso <alo_se.r.g.io@proton.me>
2025-05-27 15:00:23 +0200

    json files reformatting

--- modules/home/programs/editor/nvim/nvf/default.nix
+++ modules/home/programs/editor/nvim/nvf/default.nix
@@ -16,12 +16,13 @@
 
         autocmds = [
           {
-            enable = true;
-            desc = "Set indentation for Nix and Lua";
+            desc = "Set indentation for Nix, Lua, JSON";
             event = [ "Filetype" ];
             pattern = [
               "lua"
               "nix"
+              "json"
+              "jsonc"
             ];
             callback = lib.generators.mkLuaInline ''
               function()
@@ -30,6 +31,61 @@
                 vim.opt_local.softtabstop = 2
               end
             '';
+            enable = true;
+          }
+          {
+            desc = "Open/close in NvimTree with l/h";
+            event = [ "Filetype" ];
+            pattern = [ "NvimTree" ];
+            callback = lib.generators.mkLuaInline ''
+              function(args)
+                local api = require("nvim-tree.api")
+
+                local function opts(desc)
+                  return {
+                    desc = "nvim-tree: " .. desc,
+                    buffer = args.buf,
+                    noremap = true,
+                    silent = true,
+                    nowait = true,
+                  }
+                end
+
+                -- Custom h behavior
+                vim.keymap.set("n", "h", function()
+                  local node = api.tree.get_node_under_cursor()
+                  if not node then return end
+
+                  if node.type == "directory" and node.open then
+                    -- Close current folder if open
+                    api.node.navigate.parent_close()
+                  else
+                    -- Otherwise, close parent folder
+                    api.node.navigate.parent()
+                  end
+                end, opts("Close folder or go to parent"))
+
+                -- Custom l behavior
+                vim.keymap.set("n", "l", function()
+                  local node = api.tree.get_node_under_cursor()
+                  if not node then return end
+
+                  if node.type == "directory" then
+                    if node.open then
+                      -- Move into the directory by selecting its first child
+                      vim.cmd("normal! j")
+                    else
+                      -- Open the folder
+                      api.node.open.edit()
+                    end
+                  else
+                    -- Open the file
+                    api.node.open.edit()
+                  end
+                end, opts("Open file or enter directory"))
+              end
+            '';
+            enable = true;
           }
         ];
 
@@ -63,8 +119,24 @@
           nvimTree = {
             enable = true;
             setupOpts = {
+              actions = {
+                open_file = {
+                  eject = true;
+                  window_picker = {
+                    enable = true;
+                  };
+                };
+              };
               git = {
                 enable = true;
+              };
+              renderer = {
+                highlight_git = true;
+                icons = {
+                  show = {
+                    git = true;
+                  };
+                };
               };
             };
             mappings = {
@@ -93,6 +165,7 @@
           markdown.enable = true;
           lua.enable = true;
           java.enable = true;
+          python.enable = true;
         };
 
         statusline = {
@@ -122,6 +195,12 @@
           addDefaultGrammars = true;
           autotagHtml = true;
           fold = true;
+        };
+
+        visuals = {
+          nvim-web-devicons = {
+            enable = true;
+          };
         };
       };
     };
