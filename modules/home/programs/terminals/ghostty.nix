8dd64a0a5e68816dac35291abae5bd81c8da8a50
Sergio Alonso <alo_se.r.g.io@proton.me>
2025-05-28 18:37:16 +0200

    decrease ghostty font size, added paste shortcut

--- modules/home/programs/terminals/ghostty.nix
+++ modules/home/programs/terminals/ghostty.nix
@@ -6,10 +6,11 @@
     settings = {
       theme = "GruvboxDark";
       background-opacity = 0.85;
-      font-size = 12;
+      font-size = 10;
       keybind = [
         "ctrl+equal=increase_font_size:1"
         "ctrl+minus=decrease_font_size:1"
+        "ctrl+shift+v=paste_from_clipboard"
       ];
     };
   };
