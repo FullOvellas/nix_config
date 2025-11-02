959cb667add3e9cece522df92081d801abc01ac8
Sergio Alonso <alo_se.r.g.io@proton.me>
2025-11-02 18:53:22 +0100

    remove unused shell alias

--- modules/home/programs/shells/bash.nix
+++ modules/home/programs/shells/bash.nix
@@ -6,7 +6,6 @@
       ls = "eza";
       ll = "eza -l";
       lll = "eza -la";
-      sys-up = "sudo nix flake update && git add . && git commit -m \"flake update\" && git push && sudo nixos-rebuild switch && sudo nix-collect-garbage --delete-old";
     };
     bashrcExtra = "export EDITOR=nvim";
   };
