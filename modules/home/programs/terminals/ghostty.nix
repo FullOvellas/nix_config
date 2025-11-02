88aff17bd97591a665abf68ba5b1f2bf68e28084
Sergio Alonso <alo_se.r.g.io@proton.me>
2025-11-02 18:54:43 +0100

    ghostty visual changes

--- modules/home/programs/terminals/ghostty.nix
+++ modules/home/programs/terminals/ghostty.nix
@@ -4,14 +4,15 @@
     clearDefaultKeybinds = true;
     installBatSyntax = true;
     settings = {
-      theme = "GruvboxDark";
-      background-opacity = 0.85;
+      theme = "Gruvbox Dark";
+      background-opacity = 0.9;
       font-size = 10;
       keybind = [
         "ctrl+equal=increase_font_size:1"
         "ctrl+minus=decrease_font_size:1"
         "ctrl+shift+v=paste_from_clipboard"
       ];
+      custom-shader = "${./cursor_smear.glsl}";
     };
   };
 }
